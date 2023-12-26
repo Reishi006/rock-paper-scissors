e6ae11cf2ae2ad14ba7fd04b96fb2a1213965b03
Reishi006 <38853186+Reishi006@users.noreply.github.com>
2023-12-26 20:56:38 +0100

    Update Game.js
    probably finished the app and rearranged elements into components

--- src/Game.js
+++ src/Game.js
@@ -2,78 +2,133 @@ import './App';
 import {useState} from 'react';
 
 
-function Score({itemsArray, playerScore, opponentScore, handleScore}) {
-    let draw = Math.floor(Math.random() * 3);
 
-    const opponentArray = itemsArray;
-    const playerArray = itemsArray;
+function Round({pick, score, opScore, setPick, setScore, setOpScore}) {
+    let choice = [
+        'rock',
+        'paper',
+        'scissors'
+    ];
 
-    return (
-        <>
-            <h3>
-                <ul className="score">
-                    <li>You</li>
-                    <li>vs</li>
-                    <li>Opponent</li>
-                </ul>
-            </h3>
-            <ul className="score">
-                <li>{playerScore}</li>
-                <li> : </li>
-                <li>{opponentScore}</li>
-            </ul>
-            <ul className="score">
-                <li><Option itemName={playerArray[0]}></Option></li>
-                <li>|</li>
-                <li><Option itemName={opponentArray[draw]}></Option></li>
-            </ul>
-        </>
-    );
-}
+    let random = Math.floor(Math.random() * 3);
 
-function Option({itemName}) {
+    const calculateWinner = () => {
+        if (score < 3 && opScore < 3) {
+            if (pick === choice[random]) {
+                console.log(`${choice[random]}`);
+                random = Math.floor(Math.random() * 3);
+            } else if (pick === 'rock' && choice[random] === 'paper') {
+                setOpScore(opScore + 1);
+            } else if (pick === 'rock' && choice[random] === 'scissors') {
+                setScore(score + 1);
+            } else if (pick === 'paper' && choice[random] === 'rock') {
+                setScore(score + 1);
+            } else if (pick === 'paper' && choice[random] === 'scissors') {
+                setOpScore(opScore + 1);
+            } else if (pick === 'scissors' && choice[random] === 'rock') {
+                setOpScore(opScore + 1);
+            } else if (pick === 'scissors' && choice[random] === 'paper') {
+                setScore(score + 1);
+            }
+        } else return;
+    };
 
-    itemName = 'play';
-
-    return (
-        <div className={itemName}>{itemName}</div>
-    );
-}
-
-function GameButton({itemsArray}) {
-
-    const handleScore = (it) => {
-        console.log(it);
-    }
-
-    const returnButtons = itemsArray?.map((item) => {
-        return (
-            <button className='game-btn' key={item} onClick={() => handleScore(item)}>{item}</button>
-        )
+    const choiceButton = choice.map((ch) =>{
+        return <button key={ch} className="game-btn" onClick={() => {
+            setPick(ch);
+            calculateWinner();
+        }}>{ch.toUpperCase()}</button>
     });
 
+
+    return (
+        <div className='main-container'>
+            <h2>Game score</h2>
+            <div className="scores-grid">
+                <h3>You</h3>
+                <h3>vs</h3>
+                <h3>Opponent</h3>
+                
+                <div>{score}</div>
+                <div>:</div>
+                <div>{opScore}</div>
+
+                <div className={pick}>{pick.toUpperCase()}</div>
+                <div></div>
+                <div className={choice[random]}>{choice[random].toUpperCase()}</div>
+            </div>
+            {choiceButton}
+        </div>
+    );
+}
+
+function PlayAgain({score, opScore, setScore, setOpScore}) {
+    let whoWon = '';
+
+    if (score === 3) {
+        whoWon = 'You win!';
+    } else if (opScore === 3) {
+        whoWon = 'Opponent won'
+    }
+
+    const resetScore = () => {
+        console.log(`reset`);
+        setScore(0);
+        setOpScore(0);
+    }
+
+
     return (
         <>
-            {returnButtons}
+            <div className='scores-grid-after'>
+                <h3>You</h3>
+                <h3>vs</h3>
+                <h3>Opponent</h3>
+                
+                <div>{score}</div>
+                <div>:</div>
+                <div>{opScore}</div>
+            </div>
+
+            <div className="who-won">
+                {whoWon}
+                <button className="start-btn" onClick={resetScore}>
+                    Play again?
+                </button>
+            </div>
         </>
     );
 }
 
 export default function Game() {
-    const [playerScore, setPlayer] = useState(0);
-    const [opponentScore, setOpponent] = useState(0);
-
-    const itemsArray = [
-        'rock',
-        'paper',
-        'scissors'
-    ];
+    const [pick, setPick] = useState('rock');
+    const [score, setScore] = useState(0);
+    const [opScore, setOpScore] = useState(0);
     
     return (
-        <div className='main-container'>
-            <h3>The Game Begins!</h3>
-            <Score itemsArray={itemsArray}></Score>
-            <GameButton itemsArray={itemsArray}></GameButton>
-        </div>
+        <>
+            {(score < 3 && opScore < 3) ?
+            <Round 
+            pick={pick}
+            setPick={setPick}
+            score={score}
+            setScore={setScore}
+            opScore={opScore}
+            setOpScore={setOpScore}
+            ></Round> 
+            : ''}
+            
+
+        
+
+            {(score === 3 || opScore === 3) ?
+            <PlayAgain
+            score={score}
+            opScore={opScore}
+            setScore={setScore}
+            setOpScore={setOpScore}
+            ></PlayAgain>
+            : ''}
+        </>
     );
 }
